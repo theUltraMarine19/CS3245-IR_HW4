5034aa8b292eb64deee92ed52e60d44785af20e5
Svilen Stefanov <svilen.ks@gmail.com>
2018-04-02 23:26:58 +0800

    change variable names

--- boolean_retrieval.py
+++ boolean_retrieval.py
@@ -1,44 +1,42 @@
-def get_postings(term, dict1, postings):
+def get_postings(term, dictionary, fp_postings):
     """
     This method returns the postings for a specific term from either dict1, dict2 or positional indexing.
     :param term: term of length 1, 2 or 3
     :param dictionary:
-    :param postings:
+    :param fp_postings:
     :return: postings for the given term
     """
     # TODO: term is still a string make sure it's a list or check for word count
 
-    if (len(term) == 1)
+    if len(term) == 1:
         # check if its a single word
         # check if term in dictionary 1
-        if term in dict1:
+        if term in dictionary:
             # TODO: if we don't have enough docIDs in postings for a given term, check more synonyms
             # if not in dict 1, call synonyms and check for each of the top synonym if in dict 1
             # else get postings for term from dictionary 1 from postings.txt
-            fp_postings.seek(dict1[term]['H'])
-            postings_string = fp_postings.read(dict1[term]['T'] - dict1[term]['T'])
-            postings_list = postings_string.split(' ')
+            fp_postings.seek(dictionary[term]['H'])
+            postings_string = fp_postings.read(dictionary[term]['T'] - dictionary[term]['T'])
+            postings_list = postings_string.split()
 
-    elif (len(term) == 2)
+    elif len(term) == 2:
         # for terms of length 2, use the format of double indexing in dict'
         # check if term in dictionary 2
-        if term[0] in dict1:
-            if term[1] in dict1[term[0]]:
+        if term[0] in dictionary:
+            if term[1] in dictionary[term[0]]:
                 # TODO: if we don't have enough docIDs in postings for a given term, check more synonyms
                 # TODO: since length 2, fist check synonyms for the first word, if not enough docIDs, check synonyms for 2. word
                 # if not in dict 2, call synonyms and check for each of the top synonym if in dict 2
                 # else get postings for term from dictionary 2 from postings.txt
-                fp_postings.seek(dict1[term[0]][term[1]]['H'])
-                postings_string = fp_postings.read(dict1[term[0]][term[1]]['T'] - dict1[term[0]][term[1]]['H'])
-                postings_list = postings_string.split(' ')
+                fp_postings.seek(dictionary[term[0]][term[1]]['H'])
+                postings_string = fp_postings.read(dictionary[term[0]][term[1]]['T'] - dictionary[term[0]][term[1]]['H'])
+                postings_list = postings_string.split()
     
-    elif (len(term) == 3)
+    elif len(term) == 3:
         # complicated
         # make use of positional indexes for fetching the postings
     else
         # throw an error
-    return []
-
     # if successfully reaches here without error, return fetched postings list
     return postings_list
 
@@ -71,12 +69,12 @@ def merge_lists(l1, l2):
 
 # TODO: Can we have boolean retrieval and free text in one query
 
-def order_by_size(term, dictionary, postings):
+def order_by_size(term, dictionary, fp_postings):
     """
     Evaluates the size of the posting list of a given expression (if existing in the term dictionary).
     :param term:
     :param dictionary:
-    :param postings:
+    :param fp_postings:
     :return: 0 - if not in the term dictionary
              document frequency of the term - if the term is present in the dictionary
     """
@@ -107,17 +105,17 @@ def order_by_size(term, dictionary, postings):
 
 # TODO: define get_synonyms as a new file or as a method
 
-def bool_retrieve(query, dictionary, postings):
+def bool_retrieve(query, dictionary, fp_postings):
     """
     The main method for the boolean retrieval.
     The smallest sized list of postings should be merged first (orderBySize)
     :param query: a list of query terms
     :param dictionary:
-    :param postings:
+    :param fp_postings:
     :return: return the result of all relevant docIDs
     """
     res = []
     for term in query:
-        term_postings = get_postings(term.split(), dictionary, postings)
+        term_postings = get_postings(term.split(), dictionary, fp_postings)
         res = merge_lists(res, term_postings)
     return res
