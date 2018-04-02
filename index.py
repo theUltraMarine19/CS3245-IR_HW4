a8ba7009f253425f0f963bb5e5e5e97bf9efaef0
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-04-02 13:13:13 +0800

    index.py: Write ngram dictionary

--- index.py
+++ index.py
@@ -19,16 +19,19 @@ all_doc_ids = []
 ps = PorterStemmer()
 
 
+# params:
 # -i dataset.csv -u unidict.txt --uni-postings unipostings.txt -b bidict.txt --bi-postings bipostings.txt
+# not implemented yet:
+# -p posdict.txt --pos-postings pospostings.txt- m metadict.txt
+
 def usage():
     print "usage: " + sys.argv[0] + " -i dataset_file -u unigram-dictionary-file --uni-postings unigram-postings-file "\
                                     "-b bigram-dictionary-file --bi-postings bigram-postings-file"
 
-
 dataset_file = output_uni_dict = output_uni_postings = output_bi_dict = output_bi_postings = None
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'i:u:b:', ["uni-postings", "bi-postings"])
+    opts, args = getopt.getopt(sys.argv[1:], 'i:u:b:', ["uni-postings=", "bi-postings="])
 except getopt.GetoptError, err:
     usage()
     sys.exit(2)
@@ -47,14 +50,10 @@ for o, a in opts:
     else:
         assert False, "unhandled option"
 
-
-print dataset_file, output_uni_dict, output_uni_postings, output_bi_dict, output_bi_postings
-
-# TODO Need to parse --flags properly
-# if dataset_file is None or output_uni_dict is None or output_uni_postings is None \
-#         or output_bi_dict is None or output_bi_postings is None:
-#     usage()
-#     sys.exit(2)
+if dataset_file is None or output_uni_dict is None or output_uni_postings is None \
+        or output_bi_dict is None or output_bi_postings is None:
+    usage()
+    sys.exit(2)
 
 
 def read_data_files_test(input_dir):
@@ -63,22 +62,20 @@ def read_data_files_test(input_dir):
     :param input_dir: the path of the directory containing the training data set
     :return: None
     """
-    with open('dataset.csv','rb') as csv_file:
+    with open(input_dir, 'rb') as csv_file:
         data_reader = csv.reader(csv_file, delimiter=',', )
         for index, row in enumerate(data_reader):
-            if index == 1:
+            if index == 0:
                 continue
-            if index >= 50:
+            if index >= 5:
                 break
             doc_id = row[0]
             title = row[1]
             content = row[2]
             date_posted = row[3]
             court = row[4]
-            build_unigram_dict()
-            build_bigram_dict()
-
-
+            build_unigram_dict(doc_id, content)
+            build_bigram_dict(doc_id, content)
 
 
 def read_data_files(input_dir):
@@ -87,16 +84,18 @@ def read_data_files(input_dir):
     :param input_dir: the path of the directory containing the training data set
     :return: None
     """
-    with open('dataset.csv','rb') as csv_file:
+    with open(input_dir, 'rb') as csv_file:
         data_reader = csv.reader(csv_file, delimiter=',', )
         for index, row in enumerate(data_reader):
-            doc_id = row[0]
+            if index == 0:
+                continue
+            doc_id = int(row[0])
             title = row[1]
             content = row[2]
             date_posted = row[3]
             court = row[4]
-            build_unigram_dict()
-            build_bigram_dict()
+            build_unigram_dict(doc_id, content)
+            build_bigram_dict(doc_id, content)
 
 
 def build_unigram_dict(doc_id, doc_string):
@@ -106,13 +105,22 @@ def build_unigram_dict(doc_id, doc_string):
     :param doc_string: the text of document corresponding to the given doc_id
     :return: None
     """
+    sentences = sent_tokenize(doc_string)
+    for sent in sentences:
+        words = word_tokenize(sent)
+        for word in words:
+            term = re.sub(r'[^a-zA-Z0-9]', '', str(word))
+            term = ps.stem(term.lower())
+            if len(term) != 0:
+                if term in unigram_dict:
+                    if doc_id in unigram_dict[term]:
+                        unigram_dict[term][doc_id] += 1
+                    else:
+                        unigram_dict[term][doc_id] = 1
+                else:
+                    unigram_dict[term] = {}
+                    unigram_dict[term][doc_id] = 1
 
-def build_unigram_count_dict(term='', head=0, tail=0, freq=0):
-    """
-    Build the dictionary with term as keys and the frequency count, head and tail byte location as values.
-    :type term: the term to be added to the dictionary
-    :return: None
-    """
 
 def build_bigram_dict(doc_id, doc_string):
     """
@@ -122,12 +130,6 @@ def build_bigram_dict(doc_id, doc_string):
     :return: None
     """
 
-def build_bigram_count_dict(term='', head=0, tail=0, freq=0):
-    """
-    Build the dictionary with term as keys and the frequency count, head and tail byte location as values.
-    :type term: the term to be added to the dictionary
-    :return: None
-    """
 
 def build_positional_index_dict(doc_id, doc_string):
     """
@@ -155,12 +157,46 @@ def build_positional_index_dict(doc_id, doc_string):
                     positional_dict[term][doc_id] = [count]
             count += 1
 
-def write_output():
+
+def build_ngram_count_dict(ngram_count_dict, term='', head=0, tail=0, freq=0):
     """
-    Write the term count dictionary, doc length dictionary, and the postings file to 3 distinct txt files.
+    Build the dictionary with term as keys and the frequency count, head and tail byte location as values.
+    :type term: the term to be added to the dictionary
     :return: None
     """
+    ngram_count_dict[term] = {'h': head, 't': tail, 'f': freq}
+
+def write_ngram_output(ngram_dict, ngram_count_dict, output_file_dictionary, output_file_postings):
+    """
+    Write the term count dictionary and the postings file to 2 distinct txt files.
+    :return: None
+    """
+    with open(output_file_postings, 'w') as out_postings:
+        # term_dict has term as key, doc_id_dict as value
+        # doc_id_dict has doc id as key, term frequency corresponding to the doc id as value
+        for term, doc_id_dict in ngram_dict.iteritems():
+
+            doc_id_list = doc_id_dict.keys()
+            doc_id_list.sort()
+
+            posting = []
+            for doc_id in doc_id_list:
+                posting.append(str(doc_id) + '-' + str(doc_id_dict[doc_id]))
+            # add a space at the end for distinguishing the current posting string from the next posting string
+            posting_str = " ".join(str(e) for e in posting) + " "
+
+            head = out_postings.tell()
+            out_postings.write(posting_str)
+            freq = len(doc_id_list)
+            tail = out_postings.tell()
+            build_ngram_count_dict(ngram_count_dict, term, head, tail, freq)
+
+    with open(output_file_dictionary, 'w') as out_dict:
+        all_doc_ids.sort()
+        ngram_count_dict['ALL'] = {'f': len(all_doc_ids), 'a': all_doc_ids}
+        json.dump(ngram_count_dict, out_dict)
 
 
 if __name__ == "__main__":
     read_data_files_test(dataset_file)
+    write_ngram_output(unigram_dict, unigram_count_dict, output_uni_dict, output_uni_postings)
