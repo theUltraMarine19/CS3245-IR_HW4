70175b6ecd687321af157733017e961a22c17ee1
Madhav Goel <madhav97@gmail.com>
2018-04-09 19:26:15 +0800

    bigram and trigram normalisation and dict.txt structure

--- index.py
+++ index.py
@@ -26,7 +26,9 @@ meta_count_dict = {"title":{}, "date_posted":{}, "court":{}}
 all_doc_ids = []
 ps = PorterStemmer()
 
-doc_words = dict()
+doc_words1 = dict()
+doc_words2 = dict()
+doc_words3 = dict()
 
 # the size of the training data set
 collection_size = 0
@@ -181,14 +183,31 @@ def build_ngram_dict(doc_id, doc_string):
                                 else:
                                     ngram_dictionary[term1][term2][term3] = {}
                                     ngram_dictionary[term1][term2][term3][doc_id] = 1
+
+                                if doc_id in doc_words3:
+                                    if term1 + " " + term2 + " " + term3 in doc_words3[doc_id]:
+                                        doc_words3[doc_id][term1 + " " + term2 + " " + term3] += 1
+                                    else:
+                                        doc_words3[doc_id][term1 + " " + term2 + " " + term3] = 1
+                                else:
+                                    doc_words3[doc_id] = {term1 + " " + term2 + " " + term3: 1}
+
+                        if doc_id in doc_words2:
+                            if term1 + " " + term2 in doc_words2[doc_id]:
+                                doc_words2[doc_id][term1 + " " + term2] += 1
+                            else:
+                                doc_words2[doc_id][term1 + " " + term2] = 1
+                        else:
+                            doc_words2[doc_id] = {term1 + " " + term2: 1}
+
                 # build the length add-on to the free text retrieval
-                if doc_id in doc_words:
-                    if term1 in doc_words[doc_id]:
-                        doc_words[doc_id][term1] += 1
+                if doc_id in doc_words1:
+                    if term1 in doc_words1[doc_id]:
+                        doc_words1[doc_id][term1] += 1
                     else:
-                        doc_words[doc_id][term1] = 1
+                        doc_words1[doc_id][term1] = 1
                 else:
-                    doc_words[doc_id] = {term1: 1}
+                    doc_words1[doc_id] = {term1: 1}
 
 
 def build_positional_index_dict(doc_id, doc_string):
@@ -242,7 +261,7 @@ def build_positional_index_count_dict(positional_count_dict ,term='', head=0, ta
     positional_count_dict[term] = {'H': head, 'T': tail, 'F': freq}
 
 
-def build_ngram_dictionary_count_dict(ngram_dictionary_count_dict, term='', head=0, tail=0, freq=0):
+def build_unigram_dictionary_count_dict(ngram_dictionary_count_dict, term1='', head=0, tail=0, freq=0):
     """
     Build the dictionary with term as keys and the frequency count, head and tail byte location as values.
     :param ngram_count_dict:
@@ -252,7 +271,49 @@ def build_ngram_dictionary_count_dict(ngram_dictionary_count_dict, term='', head
     :param freq:
     :return: None
     """
-    ngram_dictionary_count_dict[term] = {'H': head, 'T': tail, 'F': freq}
+    if term1 in ngram_dictionary_count_dict:
+        ngram_dictionary_count_dict[term1]['H'] = head
+        ngram_dictionary_count_dict[term1]['T'] = tail
+        ngram_dictionary_count_dict[term1]['F'] = freq
+    else:
+        ngram_dictionary_count_dict[term1] = {'H': head, 'T': tail, 'F': freq}
+
+def build_bigram_dictionary_count_dict(ngram_dictionary_count_dict, term1='', term2 ='', head=0, tail=0, freq=0):
+    """
+    Build the dictionary with term as keys and the frequency count, head and tail byte location as values.
+    :param ngram_count_dict:
+    :type term: the term to be added to the dictionary
+    :param head:
+    :param tail:
+    :param freq:
+    :return: None
+    """
+    if term1 not in ngram_dictionary_count_dict:
+        ngram_dictionary_count_dict[term1] = {}
+
+    if term2 in ngram_dictionary_count_dict[term1]:
+        ngram_dictionary_count_dict[term1][term2]['H'] = head
+        ngram_dictionary_count_dict[term1][term2]['T'] = tail
+        ngram_dictionary_count_dict[term1][term2]['F'] = freq
+    else:
+        ngram_dictionary_count_dict[term1][term2] = {'H': head, 'T': tail, 'F': freq}
+
+def build_trigram_dictionary_count_dict(ngram_dictionary_count_dict, term1='', term2 ='', term3 = '',head=0, tail=0, freq=0):
+    """
+    Build the dictionary with term as keys and the frequency count, head and tail byte location as values.
+    :param ngram_count_dict:
+    :type term: the term to be added to the dictionary
+    :param head:
+    :param tail:
+    :param freq:
+    :return: None
+    """
+    if term1 not in ngram_dictionary_count_dict:
+        ngram_dictionary_count_dict[term1] = {}
+    if term2 not in ngram_dictionary_count_dict[term1]:
+        ngram_dictionary_count_dict[term1][term2] = {}
+    ngram_dictionary_count_dict[term1][term2][term3] = {'H': head, 'T': tail, 'F': freq}
+
 
 
 def build_meta_count_dict(category='', term='', head=0, tail=0, freq=0):
@@ -269,7 +330,6 @@ def build_meta_count_dict(category='', term='', head=0, tail=0, freq=0):
 
 
 
-# TO DO - Write this new dict to output file
 def write_ngram_dict_output(ngram_dict, ngram_count_dict, output_file_dictionary, output_file_postings):
     """
     Write the term count dictionary and the postings file to 2 distinct txt files.
@@ -282,31 +342,74 @@ def write_ngram_dict_output(ngram_dict, ngram_count_dict, output_file_dictionary
     with open(output_file_postings, 'w') as out_postings:
         # term_dict has term as key, doc_id_dict as value
         # doc_id_dict has doc id as key, term frequency corresponding to the doc id as value
-        doc_norm = dict()
-        for term, doc_id_dict in ngram_dict.iteritems():
+        doc_norm1 = dict()
+        doc_norm2 = dict()
+        doc_norm3 = dict()
 
-            doc_id_list = doc_id_dict.keys()
-            doc_id_list.sort()
+        for term1, doc_id_dict1 in ngram_dict.iteritems():
+            unigram_posting = []
 
-            posting = []
-            for doc_id in doc_id_list:
-                values = [1 + math.log(i, 10) for i in doc_words[doc_id].values()]
-                norm_val = math.sqrt(sum(i ** 2 for i in values))
-                doc_norm[doc_id] = norm_val
-                posting.append(str(doc_id) + '-' + str(doc_id_dict[doc_id]))
-            # add a space at the end for distinguishing the current posting string from the next posting string
-            posting_str = " ".join(str(e) for e in posting) + " "
+            for term_or_doc_id1, dict_or_tf1 in doc_id_dict1.iteritems():
+                if type(dict_or_tf1) is dict:
+                    term2 = term_or_doc_id1
+                    bigram_posting = []
 
-            head = out_postings.tell()
-            out_postings.write(posting_str)
-            freq = len(doc_id_list)
-            tail = out_postings.tell()
-            build_ngram_dictionary_count_dict(ngram_count_dict, term, head, tail, freq)
+                    for term_or_doc_id2, dict_or_tf2 in dict_or_tf1.iteritems():
+                        if type(dict_or_tf2) is dict:
+                            # handle trigram
+                            term3 = term_or_doc_id2
+                            trigram_posting = []
+
+                            for term_or_doc_id3, dict_or_tf3 in dict_or_tf2.iteritems():
+                                if term_or_doc_id3 not in doc_norm3:
+                                    values3 = [1 + math.log(i, 10) for i in doc_words3[term_or_doc_id3].values()]
+                                    norm_val3 = math.sqrt(sum(i ** 2 for i in values3))
+                                    doc_norm3[term_or_doc_id3] = norm_val3
+                                trigram_posting.append(str(term_or_doc_id3) + '-' + str(dict_or_tf3))
+
+                            posting_str3 = " ".join(str(e) for e in trigram_posting) + " "
+
+                            head3 = out_postings.tell()
+                            out_postings.write(posting_str3)
+                            freq3 = len(trigram_posting)
+                            tail3 = out_postings.tell()
+                            build_trigram_dictionary_count_dict(ngram_count_dict, term1, term2, term3, head3, tail3, freq3)
+                        else:
+                            # handle bigram
+                            if term_or_doc_id2 not in doc_norm2:
+                                values2 = [1 + math.log(i, 10) for i in doc_words2[term_or_doc_id2].values()]
+                                norm_val2 = math.sqrt(sum(i ** 2 for i in values2))
+                                doc_norm2[term_or_doc_id2] = norm_val2
+                            bigram_posting.append(str(term_or_doc_id2) + '-' + str(dict_or_tf2))
+                    
+                    posting_str2 = " ".join(str(e) for e in bigram_posting) + " "
+
+                    head2 = out_postings.tell()
+                    out_postings.write(posting_str2)
+                    freq2 = len(bigram_posting)
+                    tail2 = out_postings.tell()
+                    build_bigram_dictionary_count_dict(ngram_count_dict, term1, term2, head2, tail2, freq2)
+
+                else:
+                    # handle unigram
+                    if term_or_doc_id1 not in doc_norm1:
+                        values1 = [1 + math.log(i, 10) for i in doc_words1[term_or_doc_id1].values()]
+                        norm_val1 = math.sqrt(sum(i ** 2 for i in values1))
+                        doc_norm1[term_or_doc_id1] = norm_val1
+                    unigram_posting.append(str(term_or_doc_id1) + '-' + str(dict_or_tf1))
+            
+            posting_str1 = " ".join(str(e) for e in unigram_posting) + " "
+
+            head1 = out_postings.tell()
+            out_postings.write(posting_str1)
+            freq1 = len(unigram_posting)
+            tail1 = out_postings.tell()
+            build_unigram_dictionary_count_dict(ngram_count_dict, term1, head1, tail1, freq1)
 
     with open(output_file_dictionary, 'w') as out_dict:
         all_doc_ids.sort()
         ngram_count_dict['N'] = collection_size
-        ngram_count_dict['DOC_NORM'] = doc_norm
+        ngram_count_dict['DOC_NORM'] = (doc_norm1, doc_norm2, doc_norm3)
         json.dump(ngram_count_dict, out_dict)
 
 
@@ -373,7 +476,7 @@ def write_meta_output(meta_dict, meta_count_dict, output_file_dictionary, output
 
 if __name__ == "__main__":
     read_data_files_test(dataset_file)
-    print(ngram_dictionary)
-    # write_ngram_dict_output(ngram_dictionary, ngram_dictionary_count_dict, output_ngram_dict, output_ngram_postings)
+    # print(ngram_dictionary)
+    write_ngram_dict_output(ngram_dictionary, ngram_dictionary_count_dict, output_ngram_dict, output_ngram_postings)
     write_positional_output(positional_dict, positional_count_dict, output_pos_dict, output_pos_postings)
     write_meta_output(meta_dict, meta_count_dict, output_meta_dict, output_meta_postings)
